be3e5f73004a75ad7939a2d6eda1b482e356857c
Derek Cannon <derektcannon@gmail.com>
2022-02-05 00:53:29 -0800

    Added gray background for non-occurring letters

--- src/components/WordRow.svelte
+++ src/components/WordRow.svelte
@@ -8,6 +8,7 @@
 	{#each Array(maxLetters) as _, index}
 		<div
 			class="letterBox"
+			class:nonOccurringLetter={score && score[index] === 0}
 			class:correctPosition={score && score[index] === 2}
 			class:correctLetter={score && score[index] === 1}
 		>
@@ -35,13 +36,21 @@
 		width: 3rem;
 		height: 3rem;
 		border: 1px solid #787c7e;
+		box-sizing: border-box;
+	}
+
+	.nonOccurringLetter {
+		background: #3a3a3c;
+		border: none;
 	}
 
 	.correctPosition {
 		background: #538d4e;
+		border: none;
 	}
 
 	.correctLetter {
 		background: #b59f3b;
+		border: none;
 	}
 </style>
